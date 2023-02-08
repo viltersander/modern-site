5fdecf6c0fec53318ddc789352055609c856107b
viltersander <sander.vilter@ametikool.ee>
2023-02-08 13:39:48 +0200

    navbar-fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar ">
+    <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt='hookbank' className="w-[124px] h-[32px] ml-5 sm:ml-10" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
@@ -33,12 +33,12 @@ const Navbar = () => {
           <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0  w-full sidebar`}
           >
 
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-end items-center flex-1" onClick={() => setToggle((prev) => !prev)}>
               {navLinks.map((nav, index) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-2'}`}
-                >
+                  className={`font-poppins font-normal cursor-pointer text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-2'}`}
+                > 
                   <a href={`#${nav.id}`}>
                     {nav.title}
                   </a>
